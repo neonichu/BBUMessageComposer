27c7957b18762a385586f5d7a2d556b471c2f37c
Boris Bügling <boris@icculus.org>
2013-07-20 09:57:12 +0200

    Initial Commit

---
+++ BBUMessageComposer/BBUAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BBUAppDelegate.h
+//  BBUMessageComposer
+//
+//  Created by Boris Bügling on 20.07.13.
+//  Copyright (c) 2013 Boris Bügling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BBUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
