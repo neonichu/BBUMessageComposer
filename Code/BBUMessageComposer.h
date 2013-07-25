e137701c045a3bd5046e5bc3ef2da5f2de3dec6b
Boris Bügling <boris@icculus.org>
2013-07-25 07:51:20 +0200

    Made header files public.

---
+++ Code/BBUMessageComposer.h
@@ -0,0 +1,10 @@
+//
+//  BBUMessageComposer.h
+//  BBUMessageComposer
+//
+//  Created by Boris Bügling on 25.07.13.
+//  Copyright (c) 2013 Boris Bügling. All rights reserved.
+//
+
+#import "BBUMessageComposeViewController.h"
+#import "BBUMessagePoster.h"
